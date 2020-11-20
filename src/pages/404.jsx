914bfbb60aa3681b76ff410a1aa8a0716beaa584
Lars Behrenberg <l.behrenberg@gmail.com>
2020-11-20 11:15:53 +0900

    style 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,31 @@
 import React from "react"
-
+import styled from "@emotion/styled"
 import { Layout } from "layout"
+
+const Container = styled.div`
+  min-height: 68vh;
+  width: 100%;
+
+  margin: 0 auto;
+
+  color: #fff;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+`
 
 const NotFoundPage = () => (
   <Layout
     seoTitle="404: Not Found"
     seoDescription="残念ですが、このページ今見つけられなかったです。"
   >
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 )
 
